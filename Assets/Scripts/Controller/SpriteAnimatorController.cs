ab6f9ebb7c2a5f4b2c6cbb8863c76d7cc7d40728
Netalichka <netalichka2@gmail.com>
2023-05-09 20:38:35 +0300

    animation

--- Assets/Scripts/Controller/SpriteAnimatorController.cs
+++ Assets/Scripts/Controller/SpriteAnimatorController.cs
@@ -1,18 +1,104 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteAnimatorController : MonoBehaviour
+namespace PlatformerMVC
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public class SpriteAnimatorController : IDisposable
     {
-        
+        private sealed class Animation
+
+
+        {
+            public AnimState Track;
+            public List<Sprite> Sprites;
+            public bool Loop;
+            public float Speed = 10;
+            public float Counter = 0;
+            public bool Sleep;
+
+            public void Update()
+            {
+                if (Sleep) return;
+                Counter += Time.deltaTime * Speed;
+                if (Loop)
+                {
+                    while (Counter > Sprites.Count)
+                    {
+                        Counter -= Sprites.Count;
+                    }
+                }
+
+                else if (Counter > Sprites.Count)
+                {
+                    Counter = Sprites.Count;
+                    Sleep = true;
+                }
+            }
+        }
+
+
+        private AnimationConfig _config;
+        private Dictionary<SpriteRenderer, Animation> _activeAnimations = new Dictionary<SpriteRenderer, Animation>();
+
+        public SpriteAnimatorController(AnimationConfig config)
+        {
+            _config = config;
+        }
+
+        public void StartAnimation(SpriteRenderer spriteRenderer, AnimState track, bool loop, float speed)
+        {
+            if(_activeAnimations.TryGetValue(spriteRenderer, out var animation))
+            {
+                animation.Loop = loop;
+                animation.Speed = speed;
+                animation.Sleep = false;
+
+                if(animation.Track != track)
+                {
+                    animation.Track = track;
+                    animation.Sprites = _config.Sequences.Find(sequance => sequance.Track == track).Sprites;
+                    animation.Counter = 0;
+                }
+            }
+
+            else
+            {
+                _activeAnimations.Add(spriteRenderer, new Animation()
+                {
+                    Track = track,
+                    Sprites = _config.Sequences.Find(sequance => sequance.Track == track).Sprites,
+                    Loop = loop,
+                    Speed = speed
+                });
+            }
+        }
+
+        public void StopAnimation(SpriteRenderer sprite)
+        {
+            if (_activeAnimations.ContainsKey(sprite))
+            {
+                _activeAnimations.Remove(sprite);
+            }
+        }
+        public void Update()
+        {
+            foreach(var animation in _activeAnimations)
+            {
+                animation.Value.Update();
+                if(animation.Value.Counter < animation.Value.Sprites.Count)
+                {
+                    animation.Key.sprite = animation.Value.Sprites[animation.Value.Counter];
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _activeAnimations.Clear();
+        }
     }
 }
