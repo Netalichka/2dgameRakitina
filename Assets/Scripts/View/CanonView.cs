0ceabd1243db69dd0d16655b2d7cb15390101026
Netalichka <netalichka2@gmail.com>
2023-05-19 15:41:20 +0300

    rigidbullets

---
+++ Assets/Scripts/View/CanonView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+
+    public class CanonView : MonoBehaviour
+    {
+        public Transform _muzzleT;
+        public Transform _emitterT;
+        public List<LevelObjectView> _bullets;
+    }
+}
